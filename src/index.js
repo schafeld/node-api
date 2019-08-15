2b70ab86d15ca7dc2affa5d81fa309f88d37c3b9
Oliver Schafeld <oliver@schafeld.com>
2019-08-15 15:25:32 +0200

    set up Express

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
 import 'dotenv/config'
+import express from 'express'
 
-console.log('Hi folks! The secret is ' + process.env.MY_SECRET);
+const app = express()
+const port = 3000
 
+console.log('App listening on port ' +  port)
+
+app.listen(port, () =>
+  console.log('Hi folks! The secret is ' + process.env.MY_SECRET),
+)
+
+  
