db5d47d19327093462b38997c21a584b7f62e7dd
Oliver Schafeld <oliver@schafeld.com>
2019-08-15 15:47:46 +0200

    add route

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ const port = 3000
 
 console.log('App listening on port ' +  port)
 
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+})
+
 app.listen(port, () =>
   console.log('Hi folks! The secret is ' + process.env.MY_SECRET),
 )
