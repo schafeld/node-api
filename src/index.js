533f12baccbf462940598c298eb41fbeb2b90499
Oliver Schafeld <oliver@schafeld.com>
2019-08-16 11:28:00 +0200

    add example route types

--- src/index.js
+++ src/index.js
@@ -7,8 +7,20 @@ const app = express()
 app.use(cors())
 
 app.get('/', (req, res) => {
-  res.send('Hello Root!');
-})
+  return res.send('Received a GET HTTP method');
+});
+
+app.post('/', (req, res) => {
+  return res.send('Received a POST HTTP method');
+});
+
+app.put('/', (req, res) => {
+  return res.send('Received a PUT HTTP method');
+});
+
+app.delete('/', (req, res) => {
+  return res.send('Received a DELETE HTTP method');
+});
 
 app.listen(process.env.PORT, () =>
   console.log(`App is listening on port ${process.env.PORT}`),
